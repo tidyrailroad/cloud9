cf490e826630e04a5ab1f9cd8c616f60e4a229db
Emory Merryman <emory.merryman@gmail.com>
2016-11-05 22:28:15 +0000

    fixed typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ dnf update --assumeyes &&
     mkdir /opt/cloud9/c9sdk &&
     git -C /opt/cloud9/c9sdk init &&
     git -C /opt/cloud9/c9sdk remote add origin git://github.com/c9/core.git &&
-    git -C /opt//cloud9/cc9sdk pull origin master &&
+    git -C /opt//cloud9/c9sdk pull origin master &&
     /opt/cloud9/c9sdk/scripts/install-sdk.sh &&
     curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash &&
     cp /opt/cloud9/docker.repo /etc/yum.repos.d/ &&
