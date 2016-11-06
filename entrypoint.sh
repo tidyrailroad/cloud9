b49944030c259c91453764bc9b4d6cce79600baa
Emory Merryman <emory.merryman@gmail.com>
2016-11-06 00:26:22 +0000

    must have command for docker exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo docker exec --interactive --tty ${CONTAINER_ID} >> ${HOME}/.bash_profile &&
+echo docker exec --interactive --tty ${CONTAINER_ID} sh >> ${HOME}/.bash_profile &&
     node /opt/cloud9/c9sdk/server.js --listen 0.0.0.0 --auth user:password -p 8080 -w /workspace/* &&
     true
