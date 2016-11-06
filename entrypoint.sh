2c713d3d7c8a1dd82fa90906e74e163ea16db175
Emory Merryman <emory.merryman@gmail.com>
2016-11-06 19:44:08 +0000

    Restarting with simpler concept.  The user supplies the operating system. Cloud9 runs on fedora:24, but that is invisible to the user. See alpine.sh for an example.

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln --symbolic --force /workspace /usr/local/src/${PROJECT_NAME} &&
+    mkdir ${HOME}/bin &&
+    echo -en "#!/bin/sh\n\n${PROJECT_COMMAND}" >> ${HOME}/bin/shell.sh &&
+    chmod 0500 ${HOME}/bin/shell.sh &&
+    chsh -s ${HOME}/bin/shell.sh root &&
+    chsh -s ${HOME}/bin/shell.sh &&
+    node /opt/cloud9/c9sdk/server.js --listen 0.0.0.0 --auth user:password -p 8080 -w /usr/local/src/${PROJECT_NAME} &&
+    true
