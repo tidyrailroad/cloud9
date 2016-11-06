fb99a5209fdab34fd5356aedef69147febb9338c
Emory Merryman <emory.merryman@gmail.com>
2016-11-06 02:06:51 +0000

    fix error in entrypoint, directory already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-mkdir /usr/local/src &&
-    ln --symbolic --force /workspace /usr/local/src/${PROJECT_NAME} &&
+ln --symbolic --force /workspace /usr/local/src/${PROJECT_NAME} &&
     echo ${PROJECT_COMMAND} >> ${HOME}/.bash_profile &&
     node /opt/cloud9/c9sdk/server.js --listen 0.0.0.0 --auth user:password -p 8080 -w /usr/local/src/${PROJECT_NAME} &&
     true
