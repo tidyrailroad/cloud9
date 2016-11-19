a6c261fdcc6757d9e34cc73a36ea275a32661bd4
Emory Merryman <emory.merryman@deciphernow.com>
2016-11-19 07:21:33 -0500

    do not use yum

--- run.sh
+++ run.sh
@@ -28,7 +28,6 @@ git -C /opt/cloud9/c9sdk remote add origin git://github.com/c9/core.git &&
 git -C /opt/cloud9/c9sdk pull origin master &&
 /opt/cloud9/c9sdk/scripts/install-sdk.sh &&
 curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash &&
-cp /opt/cloud9/docker.repo /etc/yum.repos.d/ &&
 apk add docker &&
 apk add util-linux-user &&
 true
