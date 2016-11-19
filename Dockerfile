e19b9c336ddebcaf9e95d2896becc8baf361f26c
Emory Merryman <emory.merryman@deciphernow.com>
2016-11-19 13:50:13 -0500

    simplify

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM fedora:24
+FROM alpine:3.4
+MAINTAINER Emory Merryman emory.merryman@gmail.com
 COPY docker.repo run.sh entrypoint.sh /opt/cloud9/
-RUN ["/usr/bin/sh", "/opt/cloud9/run.sh"]
-ENTRYPOINT ["/usr/bin/sh", "/opt/cloud9/entrypoint.sh"]
+RUN ["/bin/sh", "/opt/cloud9/run.sh"]
+ENTRYPOINT ["/bin/sh", "/opt/cloud9/entrypoint.sh"]
 CMD []
-EXPOSE 8080
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir ${HOME}/bin &&
-    echo -en "#!/bin/sh\n\n${PROJECT_COMMAND}" >> ${HOME}/bin/shell.sh &&
-    chmod 0500 ${HOME}/bin/shell.sh &&
-    chsh -s ${HOME}/bin/shell.sh root &&
-    chsh -s ${HOME}/bin/shell.sh &&
-    node /opt/cloud9/c9sdk/server.js --listen 0.0.0.0 --auth user:password -p 8080 -w /usr/local/src/${PROJECT_NAME} &&
-    true
+[ -d /etc/init.d ] &&
+ls -1 /etc/init.d | while read SCRIPT
+do
+    [ -f /etc/init.d/${SCRIPT} /bin/sh /etc/init.d/${SCRIPT} || exit 64
+done &&
+node /opt/cloud9/c9sdk/server.js ${@} &&
+true
